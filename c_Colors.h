594d71e418250d497903c144b97468cb0b7e4170
margaridarro <61365669+margaridarro@users.noreply.github.com>
2020-05-06 14:32:24 +0100

    Create c_Colors.h

---
+++ c_Colors.h
@@ -0,0 +1,24 @@
+#ifndef UNTITLED13_COLORS_H
+#define UNTITLED13_COLORS_H
+
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWN 6
+#define LIGHTGRAY 7
+#define DARKGRAY 8
+#define LIGHTBLUE 9
+#define LIGHTGREEN 10
+#define LIGHTCYAN 11
+#define LIGHTRED 12
+#define LIGHTMAGENTA 13
+#define YELLOW 14
+#define WHITE 15
+
+void setColor(int textColor, int bgColor);
+
+
+#endif //UNTITLED13_COLORS_H
