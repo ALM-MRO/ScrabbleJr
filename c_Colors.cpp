b0736d0e98251c49f736cbc9681bed7fdba3d6ba
aluisamarques <58093498+aluisamarques@users.noreply.github.com>
2020-05-12 13:39:14 +0100

    Delete c_Colors.cpp

--- c_Colors.cpp
+++
@@ -1,7 +0,0 @@
-#include "Colors.h"
-#include <iostream>
-#include <Windows.h>
-
-void setColor(int textColor, int bgColor) {
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (textColor + (bgColor * 16)));
-}
