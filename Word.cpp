21a2631763481a78e65fa3b81f6473c970481ba3
aluisamarques <58093498+aluisamarques@users.noreply.github.com>
2020-05-12 13:38:40 +0100

    Delete Word.cpp

--- Word.cpp
+++
@@ -1,8 +0,0 @@
-#include "Word.h"
-
-Word:: Word(string word, char line, char column, char direction){
-    this -> line = line;
-    this -> column = column;
-    this -> word = word;
-    this -> direction = direction;
-};
